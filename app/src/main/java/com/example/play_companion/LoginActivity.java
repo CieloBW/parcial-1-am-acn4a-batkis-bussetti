f2b8282c2d76a35e7da2a8dc86680b520c1e5604
CieloBW <cielobw@gmail.com>
2023-11-28 20:52:51 -0300

    feat: login with email and password

--- app/src/main/java/com/example/play_companion/LoginActivity.java
+++ app/src/main/java/com/example/play_companion/LoginActivity.java
@@ -1,14 +1,54 @@
 package com.example.play_companion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void submitForm(View v) {
+        EditText emailInput = findViewById(R.id.editTextTextEmail);
+        EditText passwordInput = findViewById(R.id.editTextTextPassword);
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        this.login(email, password);
+    }
+
+    public void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
