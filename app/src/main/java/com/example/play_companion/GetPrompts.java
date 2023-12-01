bf753c01cc195f6501b129f9068fa2594cddf834
JuanB <juanbatkis@gmail.com>
2023-12-01 19:53:06 -0300

    feat: api response

---
+++ app/src/main/java/com/example/play_companion/GetPrompts.java
@@ -0,0 +1,44 @@
+package com.example.play_companion;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class GetPrompts extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String url = strings[0];
+        String response = "";
+        try {
+            response = run(url);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Log.i("probando_url", s);
+    }
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
--- app/src/main/java/com/example/play_companion/PromptsActivity.java
+++ app/src/main/java/com/example/play_companion/PromptsActivity.java
@@ -2,21 +2,37 @@ package com.example.play_companion;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.ViewGroup;
 
 public class PromptsActivity extends AppCompatActivity {
 
+    Button rollButton;
+    Spinner pcSpinner;
+    TextView promptText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompts);
 
-        Spinner pcSpinner = findViewById(R.id.pc_spinner);
+        checkConnection();
+
+        rollButton = findViewById(R.id.pc_roll_button);
+        promptText = findViewById(R.id.pc_prompt_text);
+        pcSpinner = findViewById(R.id.pc_spinner);
 
         String[] options = {"Movies", "Animals", "Objects", "Places"};
 
@@ -31,9 +47,47 @@ public class PromptsActivity extends AppCompatActivity {
             }
         };
 
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPrompt(pcSpinner.getSelectedItem().toString().toLowerCase());
+            }
+        });
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         pcSpinner.setAdapter(adapter);
+    }
+
+    public void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Log.d("TAG", "Has internet");
+        } else {
+            Log.d("TAG", "No internet");
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void getPrompt(String cat) {
+        GetPrompts getPrompts = new GetPrompts();
+        getPrompts.execute("https://playcompanion.juanbatkis.repl.co/get-prompt?cat=" + cat);
+
+        //int i = random.nextInt(diceSelected)+1;
+
+        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.pc_rotate);
+
+        promptText.startAnimation(rotate);
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                diceNumber.setText(String.valueOf(i));
+            }
+        }, 500);*/
     }
 
     public void returnMain(View view) {
