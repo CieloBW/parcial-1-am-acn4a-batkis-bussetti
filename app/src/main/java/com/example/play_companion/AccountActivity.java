3872cb16bec881a48e57c9b665dac63c95e5913d
CieloBW <cielobw@davinci.edu.ar>
2023-12-20 01:34:08 -0300

    feat: add user class and set account data

--- app/src/main/java/com/example/play_companion/AccountActivity.java
+++ app/src/main/java/com/example/play_companion/AccountActivity.java
@@ -1,22 +1,64 @@
 package com.example.play_companion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AccountActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String uid = currentUser.getUid();
+        db.collection("users")
+                .whereEqualTo("uid", uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document: task.getResult()) {
+                                user = document.toObject(User.class);
+
+                                TextView email = findViewById(R.id.editTextTextEmail);
+                                email.setText(user.getEmail());
+
+                                TextView firstName = findViewById(R.id.editTextTextFName);
+                                firstName.setText(user.getFirst_name());
+
+                                TextView lastName = findViewById(R.id.editTextTextLName);
+                                lastName.setText(user.getLast_name());
+                            }
+                        }
+                    }
+                });
     }
 
     public void submitLogout(View v) {
